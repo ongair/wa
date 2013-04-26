70f32c9c78c3f9ab3be016ee81adc7d1f24f41cb
Karol Sarnacki <sodercober@gmail.com>
2013-04-26 11:40:40 +0200

    Revert back to older version of Android App tokens

--- lib/whatsapp/protocol/auth_node.rb
+++ lib/whatsapp/protocol/auth_node.rb
@@ -8,6 +8,7 @@ module WhatsApp
       def initialize(number)
         super('auth', {
             # passive:   'true', # It was in sniffed packets, but receipt akcs do not work when it's on
+            passive:   'true',
             xmlns:     'urn:ietf:params:xml:ns:xmpp-sasl',
             mechanism: 'WAUTH-1',
             user:      number
