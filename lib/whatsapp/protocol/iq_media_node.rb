2a54b4907247b61addc63c5a71da06493ffe12fc
Karol Sarnacki <sodercober@gmail.com>
2013-06-11 14:50:40 +0200

    Add media iq nodes to messages queue

--- lib/whatsapp/protocol/iq_media_node.rb
+++ lib/whatsapp/protocol/iq_media_node.rb
@@ -4,8 +4,14 @@ module WhatsApp
   module Protocol
 
     class IqMediaNode < Node
+      attr_reader :fingerprint, :type, :size, :message_id
 
       def initialize(fingerprint, type, size, message_id = Util::IdGenerator.next)
+        @fingerprint = fingerprint
+        @type        = type
+        @size        = size
+        @message_id  = message_id
+
         media_node = Node.new('media', {
             xmlns: 'w:m',
             hash:  fingerprint,
