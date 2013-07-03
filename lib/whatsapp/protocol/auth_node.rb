b39f23a7e7f9adb9136e833047013e0fbf372428
Karol Sarnacki <sodercober@gmail.com>
2013-07-03 13:29:20 +0200

    Always include "passive" option in Auth Node

--- lib/whatsapp/protocol/auth_node.rb
+++ lib/whatsapp/protocol/auth_node.rb
@@ -8,12 +8,11 @@ module WhatsApp
       # Receipt akcs do not work when in passive mode, but there are a lot of server-delivery-timeouts when it's off
       def initialize(number, passive = false)
         attributes = {
+            passive:   !!passive,
             xmlns:     'urn:ietf:params:xml:ns:xmpp-sasl',
             mechanism: 'WAUTH-1',
             user:      number
         }
-
-        attributes[:passive] = 'true' if passive
 
         super('auth', attributes)
       end
