aca6e3ec004816979911c4cf92fc374a31b84912
Lukasz Pawela <lukasz.pawela@silequipe.com>
2014-04-01 02:42:23 +0200

    New id generation

--- lib/whatsapp/util/id_generator.rb
+++ lib/whatsapp/util/id_generator.rb
@@ -15,11 +15,12 @@ module WhatsApp
       end
 
       def next(prefix = '')
-        now = Time.new.to_f.to_s.split('.', 2)
-
+        #now = Time.new.to_f.to_s.split('.', 2)
+        now = Time.new.to_i.to_s
         @id += 1
 
-        "#{prefix}#{now[0]}-#{@id}#{now[1][0, 3]}#{100 + rand(900)}"
+        #"#{prefix}#{now[0]}-#{@id}#{now[1][0, 3]}#{100 + rand(900)}"
+        "#{prefix}#{now}-#{@id}"
       end
     end
 
